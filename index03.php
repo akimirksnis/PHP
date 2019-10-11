cb1c380886a713a45ce865eaa661093d3c741b0e
Akimirksnis <Akimirksnis>
2019-10-11 22:46:20 +0300

    aritmetika

---
+++ index03.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php3</title>
+</head>
+
+<body>
+<?php
+$x = 10;
+$y = 7;
+$result = $x + $y;
+    echo "$x + $y = $result </br>";
+    $result = $x - $y;
+    echo "$x - $y = $result </br>";
+    $result = $x * $y;
+    echo "$x * $y = $result </br>";
+    $result = $x / $y;
+    echo "$x / $y = $result </br>";
+    $result = $x % $y;
+    echo "$x % $y = $result </br>";
+?>
+</body>
+</html>
+
