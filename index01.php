996d54f1aa732ad746fa5292281613458dc727f5
Akimirksnis <Akimirksnis>
2019-10-11 22:04:46 +0300

    1 uzduotis

---
+++ index01.php
@@ -0,0 +1,10 @@
+<? php
+$salis = 'Lietuva';
+$miestas = 'Vilnius';
+$populiacija = 574221;
+?>
+<ul>
+    <li><?php echo $salis;?></li>
+    <li><?php echo $miestas;?></li>
+    <li><?php echo $populiacija;?></li>
+</ul>
