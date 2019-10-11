d9fcbd3b05742479279360c6827249e6690aecc9
Akimirksnis <Akimirksnis>
2019-10-11 22:40:16 +0300

    uzduotis02

---
+++ index02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php02</title>
+</head>
+
+<body>
+<?php
+$miestas = 'Vilnius';
+$salis= 'Lietuva';
+?>
+<p><?php echo "$miestas - Lietuvos sostine"?></p>
+<p><?php echo "$miestas ir $salis sparciai auga"?></p>
+
+</body>
+</html>
