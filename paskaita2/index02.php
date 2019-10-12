7d2882dcb3e71762ca0686d41bc9eeebef184023
Akimirksnis <Akimirksnis>
2019-10-12 12:41:41 +0300

    array

---
+++ paskaita2/index02.php
@@ -0,0 +1,25 @@
+<?php
+$cities3 = [
+    'Tokijas'=>[13.6,1868, 'Japonija'],
+    'Vasingtonas'=>[0.6,1790, 'Jav'],
+    'Maskva'=>[11.5,1147, 'Rusija']];
+$cities3 ['Londonas'] = [8.6,43, 'Anglija'];
+print_r ($cities3);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP05</title>
+</head>
+
+<body>
+    <ul>
+        <li><?php echo "Gyventoju skaicius: 8.6mln."?></li>
+        <li><?php echo "Ikurtas: 43m"?></li>
+        <li><?php echo "Salis: Anglija"?></li>
+    </ul>
+
+</body>
+</html>
